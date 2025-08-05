adcd6dec30ffc6aaafa4d495a55a53a80160ffd4
ByramDigitalMedia <contact@byramdigitalmedia.com>
2025-08-05 11:22:12 -0400

    first commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+/* =======================================================
+  * Template Name: West Coast Model
+  * Template URL: https://byramdigitalmedia.com
+  * Updated: Aug 4 2025 with Bootstrap  
+  * Author: BDM
+  * License: MIT
+  ======================================================== */
+
+let date = new Date().getFullYear()
+document.getElementById("year").innerHTML = date
